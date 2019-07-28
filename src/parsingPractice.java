f4cc5e9b6467fa9feabe06a4b0d6107d8d4e77ac
Iulian <iulian181@gmail.com>
2019-07-28 15:03:33 +0300

    1

--- src/parsingPractice.java
+++ src/parsingPractice.java
@@ -10,7 +10,18 @@ public class parsingPractice {
         int num = Integer.parseInt(word);
         System.out.println(num + 5);*/
 
-        int num = Integer.parseInt(JOptionPane.showInputDialog("Enter any number"));
-        System.out.println(num + 5);
+        /*int num = Integer.parseInt(JOptionPane.showInputDialog("Enter any number"));
+        System.out.println(num + 5);*/
+
+        /*char ch = '*';
+        String word = "" + ch;
+        int num = Integer.parseInt("" + ch);
+        System.out.println(num);*/
+
+        char ch = '*';
+        String word = "" + ch;
+        String cuvant = "word";
+        ch = cuvant.charAt(3);
+        System.out.println(ch);
     }
 }
